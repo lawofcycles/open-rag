833ce1f0ecb9ec4fd38a226c12d24ab60dccf777
bering <70102274+lawofcycles@users.noreply.github.com>
2023-10-24 12:03:56 +0000

    loader

--- app/multilingual-e5-large-api.py
+++ app/multilingual-e5-large-api.py
@@ -1,32 +1,74 @@
-import torch.nn.functional as F
+import os
 
-from transformers import AutoTokenizer, AutoModel
-from flask import Flask, request, jsonify
-from torch import Tensor
+from llama_index import (
+    GPTVectorStoreIndex,
+    SimpleDirectoryReader,
+    StorageContext,
+    load_index_from_storage,
+    download_loader,
+)
 
-app = Flask(__name__)
-tokenizer = AutoTokenizer.from_pretrained('intfloat/multilingual-e5-large')
-model = AutoModel.from_pretrained('intfloat/multilingual-e5-large')
+persist_dir = "./resource/211122_amlcft_guidelines.pdf"
 
-@app.route("/embeddings", methods=["POST"])
-def get_embeddings():
-    content = request.json
-    input_texts = content["text"]
-    # Tokenize the input texts
-    batch_dict = tokenizer(input_texts, max_length=512, padding=True, truncation=True, return_tensors='pt')
+CJKPDFReader = download_loader("CJKPDFReader")
 
-    outputs = model(**batch_dict)
-    embeddings = average_pool(outputs.last_hidden_state, batch_dict['attention_mask'])
+loader = CJKPDFReader()
+documents = loader.load_data(file=persist_dir)
+print(documents)
 
-    # normalize embeddings
-    embeddings = F.normalize(embeddings, p=2, dim=1)
-    scores = (embeddings[:2] @ embeddings[2:].T) * 100
-    return jsonify({"embeddings": scores.tolist()})
+# if not os.path.exists(persist_dir):
+#     os.mkdir(persist_dir)
+# documents = SimpleDirectoryReader("data").load_data()
+# index = GPTVectorStoreIndex.from_documents(documents)
+# index.storage_context.persist(persist_dir)
 
-def average_pool(last_hidden_states: Tensor,
-                 attention_mask: Tensor) -> Tensor:
-    last_hidden = last_hidden_states.masked_fill(~attention_mask[..., None].bool(), 0.0)
-    return last_hidden.sum(dim=1) / attention_mask.sum(dim=1)[..., None]
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+# from langchain.embeddings import HuggingFaceEmbeddings
+# from llama_index import GPTVectorStoreIndex, ServiceContext, LangchainEmbedding
+
+# # 埋め込みモデルの準備
+# embed_model = LangchainEmbedding(HuggingFaceEmbeddings(
+#     model_name="intfloat/multilingual-e5-large"
+# ))
+
+# # ServiceContextの準備
+# service_context = ServiceContext.from_defaults(
+#     embed_model=embed_model
+# )
+
+# # インデックスの生成
+# index = GPTVectorStoreIndex.from_documents(
+#     documents, # ドキュメント
+#     service_context=service_context, # ServiceContext
+# )
+
+
+
+
+# app = Flask(__name__)
+# tokenizer = AutoTokenizer.from_pretrained('intfloat/multilingual-e5-large')
+# model = AutoModel.from_pretrained('intfloat/multilingual-e5-large')
+
+# @app.route("/embeddings", methods=["POST"])
+# def get_embeddings():
+#     content = request.json
+#     input_texts = content["text"]
+#     # Tokenize the input texts
+#     batch_dict = tokenizer(input_texts, max_length=512, padding=True, truncation=True, return_tensors='pt')
+
+#     outputs = model(**batch_dict)
+#     embeddings = average_pool(outputs.last_hidden_state, batch_dict['attention_mask'])
+
+#     # normalize embeddings
+#     embeddings = F.normalize(embeddings, p=2, dim=1)
+#     scores = (embeddings[:2] @ embeddings[2:].T) * 100
+#     return jsonify({"embeddings": scores.tolist()})
+
+# def average_pool(last_hidden_states: Tensor,
+#                  attention_mask: Tensor) -> Tensor:
+#     last_hidden = last_hidden_states.masked_fill(~attention_mask[..., None].bool(), 0.0)
+#     return last_hidden.sum(dim=1) / attention_mask.sum(dim=1)[..., None]
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
