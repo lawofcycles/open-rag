58f001a53524078ee4ea620144bd87f0b918d0aa
bering <70102274+lawofcycles@users.noreply.github.com>
2023-10-24 15:14:40 +0000

    loader

--- app/multilingual-e5-large-api.py
+++ app/multilingual-e5-large-api.py
@@ -41,7 +41,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 system_prompt = """以下は、タスクを説明する指示と、文脈のある入力の組み合わせです。要求を適切に満たす応答を書きなさい。"""
 
 # This will wrap the default prompts that are internal to llama-index
-prompt_string = """\n\n### 指示: \n{query_str}: \n\n\n### 応答"""
+prompt_string = """\n\n###指示:\n{query_str}:\n\n\n###応答"""
 query_wrapper_prompt = PromptTemplate(prompt_string)
 
 llm = HuggingFaceLLM(
