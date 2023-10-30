34ccfc276987cbc045fa6f6612d3f5bfb3d834ff
bering <70102274+lawofcycles@users.noreply.github.com>
2023-10-30 22:47:38 +0000

    a

--- app/multilingual-e5-large-api2.py
+++ app/multilingual-e5-large-api2.py
@@ -3,98 +3,13 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
 import time
-
-import sys
-import logging
-import os
-import re
 import torch
-
-logging.basicConfig(stream=sys.stdout, level=logging.WARNING, force=True)
-from llama_index import (
-    VectorStoreIndex,
-    SimpleDirectoryReader,
-    StorageContext,
-    load_index_from_storage,
-    download_loader,
-    ServiceContext,
-    LangchainEmbedding,
-    SimpleKeywordTableIndex,
-)
-from llama_index.vector_stores import FaissVectorStore
-from transformers import (
-    AutoTokenizer, 
-    AutoModelForCausalLM, 
-    BitsAndBytesConfig,
-    pipeline,
-)
-import torch
-from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
-
-# import QueryBundle
-from llama_index import QueryBundle
-
-# import NodeWithScore
-from llama_index.schema import NodeWithScore
-
-# Retrievers
-from llama_index.retrievers import (
-    BaseRetriever,
-    VectorIndexRetriever,
-    KeywordTableSimpleRetriever,
-)
-
-from typing import List
-
-from langchain.embeddings.huggingface import HuggingFaceBgeEmbeddings
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.document_loaders import TextLoader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores import FAISS
-from langchain.chains import RetrievalQA
-from langchain.llms.huggingface_pipeline import HuggingFacePipeline
-from langchain.prompts import PromptTemplate
-import torch
-from llama_index.llms import HuggingFaceLLM
-from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, pipeline
-from langchain.llms import HuggingFacePipeline
-from llama_index.callbacks import CallbackManager, LlamaDebugHandler
-from llama_index.llms.base import ChatMessage, MessageRole
-from llama_index.prompts.base import ChatPromptTemplate
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from llama_index.node_parser import SimpleNodeParser
-from llama_index import ServiceContext
-from llama_index.callbacks import CBEventType
-from transformers import AutoTokenizer,AutoModelForCausalLM
-import torch
-
+from transformers import AutoTokenizer, AutoModelForCausalLM
 from transformers import pipeline
 from langchain.llms import HuggingFacePipeline
+from langchain.prompts import PromptTemplate
+from langchain.chains.question_answering import load_qa_chain
 
-from langchain.embeddings import HuggingFaceEmbeddings
-from llama_index import LangchainEmbedding
-from typing import Any, List
-
-from typing                      import List, Union, Optional, Type
-from pathlib                     import Path
-from llama_index                 import download_loader, GPTVectorStoreIndex, ServiceContext, OpenAIEmbedding
-from llama_index                 import PromptTemplate as LlamaIndexPromptTemplate
-from transformers                import (AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, pipeline)
-from langchain.chat_models       import ChatOpenAI
-from langchain.llms              import OpenAI, HuggingFacePipeline
-from langchain.tools             import BaseTool, StructuredTool, Tool, tool
-from langchain.agents            import AgentType, AgentExecutor, Tool, LLMSingleActionAgent, AgentOutputParser
-from langchain.schema            import OutputParserException
-from langchain.schema.agent      import AgentAction, AgentFinish
-from langchain.chains            import LLMChain, SequentialChain
-from langchain.memory            import ConversationBufferMemory, ConversationBufferWindowMemory
-from langchain.prompts           import PromptTemplate, StringPromptTemplate
-from langchain.embeddings        import HuggingFaceEmbeddings
-from langchain.callbacks         import get_openai_callback
-from langchain.callbacks.manager import (AsyncCallbackManagerForToolRun, CallbackManagerForToolRun)
-from auto_gptq import AutoGPTQForCausalLM, BaseQuantizeConfig
-from transformers import AutoTokenizer
-from langchain.document_loaders import UnstructuredFileLoader
 
 # data source
 PERSIST_DIR = "./resource/211122_amlcft_guidelines.pdf"
@@ -139,6 +54,65 @@ print(f"処理時間[s]: {elapsed_time:.2f}")
 for i in range(len(docs)):
     print(docs[i])
 
+# setup LLM
+MODEL_NAME = "elyza/ELYZA-japanese-Llama-2-7b-fast-instruct"
+
+# Tokenizer
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, trust_remote_code=True)
+
+# Model
+model = AutoModelForCausalLM.from_pretrained(
+    MODEL_NAME,
+    device_map="auto",
+    torch_dtype=torch.float16
+)
+
+pipe = pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    max_new_tokens=512,
+    do_sample=True,
+    top_k=20,
+    temperature=0.7,
+    # device=device,
+)
+llm = HuggingFacePipeline(pipeline=pipe)
+
+B_INST, E_INST = "[INST]", "[/INST]"
+B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
+DEFAULT_SYSTEM_PROMPT = "参考情報を元に、ユーザーからの質問に簡潔に正確に答えてください。"
+text = "{context}\nユーザからの質問は次のとおりです。{question}"
+template = "{bos_token}{b_inst} {system}{prompt} {e_inst} ".format(
+    bos_token=tokenizer.bos_token,
+    b_inst=B_INST,
+    system=f"{B_SYS}{DEFAULT_SYSTEM_PROMPT}{E_SYS}",
+    prompt=text,
+    e_inst=E_INST,
+)
+rag_prompt_custom = PromptTemplate(
+    template=template, input_variables=["context", "question"]
+)
+
+# チェーンの準備
+chain = load_qa_chain(llm, chain_type="stuff", prompt=rag_prompt_custom)
+
+# RAG ありの場合
+start = time.time()
+# ベクトル検索結果の上位3件と質問内容を入力として、elyzaで文章生成
+inputs = {"input_documents": docs, "question": question}
+output = chain.run(inputs)
+elapsed_time = time.time() - start
+print("RAGあり")
+print(f"処理時間[s]: {elapsed_time:.2f}")
+print(f"出力内容：\n{output}")
+print(f"トークン数: {llm.get_num_tokens(output)}")
+
+###################################################
+# メモリの解放
+
+del model, tokenizer, pipe, llm, chain
+torch.cuda.empty_cache()
 
 # # query付きのHuggingFaceEmbeddings
 # class HuggingFaceQueryEmbeddings(HuggingFaceEmbeddings):
