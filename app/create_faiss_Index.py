505fe1f0e7675053e7f7d8d82ca03f0fb909e140
bering <70102274+lawofcycles@users.noreply.github.com>
2023-10-31 22:34:29 +0000

    a

--- app/create_faiss_Index.py
+++ app/create_faiss_Index.py
@@ -2,11 +2,19 @@ from langchain.document_loaders import UnstructuredFileLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
+import argparse
+
+# Parserの作成
+parser = argparse.ArgumentParser(description='引数のPDFのパスを読み込んでFAISSのインデックスを作成する')
+
+# 引数の追加
+parser.add_argument('arg1', type=str, help='pdfのパス')
+args = parser.parse_args()
 
 # data source
 PERSIST_DIR = "./resource/211122_amlcft_guidelines.pdf"
 
-loader = UnstructuredFileLoader(PERSIST_DIR)
+loader = UnstructuredFileLoader(args.arg1)
 documents = loader.load()
 print(f"number of docs: {len(documents)}")
 print("--------------------------------------------------")
@@ -34,4 +42,4 @@ EMBED_MODEL_NAME = "intfloat/multilingual-e5-large"
 embeddings = HuggingFaceEmbeddings(model_name=EMBED_MODEL_NAME)
 
 db = FAISS.from_documents(splitted_texts, embeddings)
-db.save_local("faiss_index")
+db.save_local(args.arg1)
