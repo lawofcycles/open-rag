2725d73d2491c61f7258e4ee4fe5609e991c4d39
bering <70102274+lawofcycles@users.noreply.github.com>
2023-11-01 00:56:03 +0000

    a

--- app/e5elyzachatapp.py
+++ app/e5elyzachatapp.py
@@ -29,44 +29,7 @@ st.subheader("intfloat/multilingual-e5-largeとelyza/ELYZA-japanese-Llama-2-7b-f
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    if message["role"] == "user":
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-    else:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
 
-# Accept user input
-if myprompt := st.chat_input("ご質問をどうぞ"):
-
-    if st.session_state.source == None:
-        st.chat_input("先にファイルをアップロードしてください")
-
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": myprompt})
-    # Display user message in chat message container
-    with st.chat_message("user"):
-        st.markdown(myprompt)
-        usertext = f"user: {myprompt}"
-        writehistory(usertext)
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        full_response = ""
-        apiresponse = requests.get(f'http://127.0.0.1:8000/model?source={st.session_state.source}&question={myprompt}')
-        risposta = apiresponse.content.decode("utf-8")
-        res  =  risposta[1:-1]
-        response = res.split(" ")
-        for r in response:
-            full_response = full_response + r + " "
-            message_placeholder.markdown(full_response + "▌")
-            sleep(0.1)
-        message_placeholder.markdown(full_response)
-        asstext = f"assistant: {full_response}"
-        writehistory(asstext)
-        st.session_state.messages.append({"role": "assistant", "content": full_response})
 
 uploaded_file = st.file_uploader('Choose a source pdf')
 
@@ -105,4 +68,43 @@ if uploaded_file is not None:
         db.save_local("faiss_index/" + uploaded_file.name)
         st.session_state.source = uploaded_file.name
     st.success('indexing completed')
+
+    # Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    if message["role"] == "user":
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+    else:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+# Accept user input
+if myprompt := st.chat_input("ご質問をどうぞ"):
+
+    if st.session_state.source == None:
+        st.chat_input("先にファイルをアップロードしてください")
+
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": myprompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(myprompt)
+        usertext = f"user: {myprompt}"
+        writehistory(usertext)
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        apiresponse = requests.get(f'http://127.0.0.1:8000/model?source={st.session_state.source}&question={myprompt}')
+        risposta = apiresponse.content.decode("utf-8")
+        res  =  risposta[1:-1]
+        response = res.split(" ")
+        for r in response:
+            full_response = full_response + r + " "
+            message_placeholder.markdown(full_response + "▌")
+            sleep(0.1)
+        message_placeholder.markdown(full_response)
+        asstext = f"assistant: {full_response}"
+        writehistory(asstext)
+        st.session_state.messages.append({"role": "assistant", "content": full_response})
 
