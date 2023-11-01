ea455f4877fd41bb7f2604fd76b4931f818dc58a
bering <70102274+lawofcycles@users.noreply.github.com>
2023-11-01 00:16:01 +0000

    a

--- app/e5elyzachatapp.py
+++ app/e5elyzachatapp.py
@@ -77,5 +77,22 @@ if uploaded_file is not None:
         f.write(uploaded_file.getbuffer())
         st.success("Saved File")
 
-    # loader = UnstructuredFileLoader(uploaded_file.getvalue())
-    # documents = loader.load()
+loader = UnstructuredFileLoader(os.path.join("resource",uploaded_file.name))
+documents = loader.load()
+print(f"number of docs: {len(documents)}")
+print("--------------------------------------------------")
+text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=600,
+    chunk_overlap=20,
+    separators=["\n\n\n","\n\n","\n"],
+)
+splitted_texts = text_splitter.split_documents(documents)
+print(f"チャンクの総数：{len(splitted_texts)}")
+print(f"チャンクされた文章の確認（20番目にチャンクされたデータ）：\n{splitted_texts[20]}")
+
+# embed model
+EMBED_MODEL_NAME = "intfloat/multilingual-e5-large"
+embeddings = HuggingFaceEmbeddings(model_name=EMBED_MODEL_NAME)
+
+db = FAISS.from_documents(splitted_texts, embeddings)
+db.save_local("faiss_index/" + uploaded_file.name)
