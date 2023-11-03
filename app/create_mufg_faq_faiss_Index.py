bb02d843fe4f63dc880795309887a0daafe0b136
bering <70102274+lawofcycles@users.noreply.github.com>
2023-11-03 05:09:21 +0000

    a

--- app/create_mufg_faq_faiss_Index.py
+++ app/create_mufg_faq_faiss_Index.py
@@ -2,23 +2,38 @@ from langchain.document_loaders import UnstructuredFileLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
-from langchain.document_loaders import DirectoryLoader
 import argparse
 
 # Parserの作成
-parser = argparse.ArgumentParser(description='引数のファイルを読み込んでFAISSのインデックスを作成する')
+parser = argparse.ArgumentParser(description='引数のPDFのパスを読み込んでFAISSのインデックスを作成する')
 
 # 引数の追加
-parser.add_argument('arg1', type=str, help='ファイルパス')
+parser.add_argument('arg1', type=str, help='pdfのパス')
 parser.add_argument('arg2', type=str, help='indexの名前')
 args = parser.parse_args()
 
-loader = DirectoryLoader(args.arg1)
+# data source
+PERSIST_DIR = "./resource/211122_amlcft_guidelines.pdf"
+
+loader = UnstructuredFileLoader(args.arg1)
 documents = loader.load()
 print(f"number of docs: {len(documents)}")
 print("--------------------------------------------------")
 
-text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder()
+text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=600,
+    chunk_overlap=20,
+    separators=["\n\n\n","\n\n","\n"],
+)
+
+# text_splitter = RecursiveCharacterTextSplitter.from_huggingface_tokenizer(
+#     tokenizer,
+#     chunk_size=300,
+#     chunk_overlap=20,
+#     # separators=["\n= ", "\n== ", "\n=== ", "\n\n",
+#     #              "\n", "。", "「", "」", "！",
+#     #              "？", "、", "『", "』", "(", ")"," ", ""],
+# )
 
 splitted_texts = text_splitter.split_documents(documents)
 print(f"チャンクの総数：{len(splitted_texts)}")
