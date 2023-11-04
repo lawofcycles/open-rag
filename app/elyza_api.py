c86e600e7a448af6576ac05e30acc4410921a3d3
bering <70102274+lawofcycles@users.noreply.github.com>
2023-11-04 17:23:24 +0000

    a

--- app/elyza_api.py
+++ app/elyza_api.py
@@ -50,7 +50,7 @@ pipe = pipeline(
     do_sample=True,
     top_k=20,
     temperature=0.1,
-    repetition_penalty=20.0, 
+    repetition_penalty=1.0, 
 )
 llm = HuggingFacePipeline(pipeline=pipe)
 
