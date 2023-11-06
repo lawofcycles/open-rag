f9c5bc856c1ef064e8431bbe8fb69d61ee5f0599
bering <70102274+lawofcycles@users.noreply.github.com>
2023-11-06 20:19:53 +0000

    ref

--- app/e5elyzachatapp.py
+++ app/e5elyzachatapp.py
@@ -49,9 +49,10 @@ if myprompt := st.chat_input("ご質問をどうぞ"):
             message_placeholder.markdown(full_response + "▌")
             sleep(0.01)
         message_placeholder.markdown(full_response)
-        st.markdown("""---
-                    Vector Search Time: " %s\n
-                    Generation Time: %s\n
-                    Vector Search Result: %s\n""" % (search_time, generation_time, vector_search_result))
+        generating_info_placeholder = st.empty()
+        generating_info_placeholder.markdown(f"""---
+Vector Search Time: {search_time}\n
+Generation Time: {generation_time}\n
+Vector Search Result: {vector_search_result}\n""")
         asstext = f"assistant: {full_response}"
         st.session_state.messages.append({"role": "assistant", "content": full_response})
